76e6b97e73cf74aef4615512c223a578ace0afc4
TuoxinLi <txli299@gmail.com>
2023-09-06 15:17:10 -0400

    Add React 18 features

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import CalendarHeader from "./components/CalendarHeader";
 import Sidebar from "./components/Sidebar";
 import Month from "./components/Month";
 import GlobalContext from "./context/GlobalContext";
-// import EventModal from "./components/EventModal";
+import EventModal from "./components/EventModal";
 function App() {
   const [currenMonth, setCurrentMonth] = useState(getMonth());
   const { monthIndex, showEventModal } = useContext(GlobalContext);
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <React.Fragment>
-      {/* {showEventModal && <EventModal />} */}
+      {showEventModal && <EventModal />}
 
       <div className="h-screen flex flex-col">
         <CalendarHeader />
